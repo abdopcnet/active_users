70b0d103666e5e6348e3375ead085e184bc51ca0
abdopcnet <abdopcnet@gmail.com>
2025-11-13 02:38:30 +0300

    Refactor language change buttons to reload document before setting language

--- active_users/public/js/user_change_language.js
+++ active_users/public/js/user_change_language.js
@@ -3,16 +3,20 @@ frappe.ui.form.on("User", {
     // إذا اللغة الحالية إنجليزية، أظهر زر لتغييرها إلى العربية
     if (frm.doc.language === "en") {
       frm.add_custom_button(__("تغيير اللغة إلى العربية"), () => {
-        frm.set_value("language", "ar");
-        frm.save();
+        frm.reload_doc().then(() => {
+          frm.set_value("language", "ar");
+          frm.save();
+        });
       });
     }
 
     // إذا اللغة الحالية عربية، أظهر زر لتغييرها إلى الإنجليزية
     else if (frm.doc.language === "ar") {
       frm.add_custom_button(__("تغيير اللغة إلى الإنجليزية"), () => {
-        frm.set_value("language", "en");
-        frm.save();
+        frm.reload_doc().then(() => {
+          frm.set_value("language", "en");
+          frm.save();
+        });
       });
     }
   },
