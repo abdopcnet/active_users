4b797fc98c953cd59149427d9cf76561819255db
abdopcnet <abdopcnet@gmail.com>
2025-11-12 18:55:35 +0300

    Add missing fixture for Property Setter in Active Users module

--- active_users/hooks.py
+++ active_users/hooks.py
@@ -19,5 +19,6 @@ app_include_css = "assets/active_users/css/active_users_bundle.css"
 app_include_js = "assets/active_users/js/active_users_bundle.js"
 
 fixtures = [
-    {"doctype": "Client Script", "filters": [["module", "=", "Active Users"]]}
+    {"doctype": "Client Script", "filters": [["module", "=", "Active Users"]]},
+    {"doctype": "Property Setter", "filters": [["module", "=", "Active Users"]]},
 ]
