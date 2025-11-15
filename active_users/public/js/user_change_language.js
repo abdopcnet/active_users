7f082f24127cfdb5227716c9fd240d1ea34ab7a8
abdopcnet <abdopcnet@gmail.com>
2025-11-15 05:00:55 +0300

    Update button labels for language change to improve clarity

--- active_users/public/js/user_change_language.js
+++ active_users/public/js/user_change_language.js
@@ -2,7 +2,7 @@ frappe.ui.form.on("User", {
   refresh(frm) {
     // إذا اللغة الحالية إنجليزية، أظهر زر لتغييرها إلى العربية
     if (frm.doc.language === "en") {
-      frm.add_custom_button(__("تغيير اللغة إلى العربية"), () => {
+      frm.add_custom_button(__("English"), () => {
         frm.reload_doc().then(() => {
           frm.set_value("language", "ar");
           frm.save();
@@ -12,7 +12,7 @@ frappe.ui.form.on("User", {
 
     // إذا اللغة الحالية عربية، أظهر زر لتغييرها إلى الإنجليزية
     else if (frm.doc.language === "ar") {
-      frm.add_custom_button(__("تغيير اللغة إلى الإنجليزية"), () => {
+      frm.add_custom_button(__("العربية"), () => {
         frm.reload_doc().then(() => {
           frm.set_value("language", "en");
           frm.save();
