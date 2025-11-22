5df286984d7c9691c653438132e30e0eea0beb95
abdopcnet <abdopcnet@gmail.com>
2025-11-22 00:57:04 +0000

    🐍 active_users_has_user.py

--- active_users/active_users/doctype/active_users_has_user/active_users_has_user.py
+++ active_users/active_users/doctype/active_users_has_user/active_users_has_user.py
@@ -3,9 +3,8 @@
 # Company: Level Up Marketing & Software Development Services
 # Licence: Please refer to LICENSE file
 
-
 from frappe.model.document import Document
 
-
 class ActiveUsersHasUser(Document):
-    pass
+	pass
+
