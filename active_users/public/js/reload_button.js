fbb8c244317171322fbae0408f2578df17e665d3
abdopcnet <abdopcnet@gmail.com>
2025-12-04 15:21:22 +0200

    🔄 reload_button.js

--- active_users/public/js/reload_button.js
+++ active_users/public/js/reload_button.js
@@ -5,53 +5,62 @@
  *  Licence: Please refer to LICENSE file
  */
 
-frappe.provide("frappe._active_users.reload");
+frappe.provide('frappe._active_users.reload');
 
 // ===== SECTION: Global clearCacheAndReload helper =====
 // Clear cache and reload function
 (function () {
-  if (window.clearCacheAndReload) return;
-  window.clearCacheAndReload = function () {
-    try {
-      localStorage.clear();
-    } catch (_) {
-      try {
-        Object.keys(localStorage).forEach((k) => localStorage.removeItem(k));
-      } catch (_) {}
-    }
-    try {
-      sessionStorage.clear();
-    } catch (_) {}
-    try {
-      location.reload(true);
-    } catch (_) {
-      location.reload();
-    }
-  };
+	if (window.clearCacheAndReload) return;
+	window.clearCacheAndReload = function () {
+		try {
+			localStorage.clear();
+		} catch (_) {
+			try {
+				Object.keys(localStorage).forEach((k) => localStorage.removeItem(k));
+			} catch (_) {}
+		}
+		try {
+			sessionStorage.clear();
+		} catch (_) {}
+		try {
+			location.reload(true);
+		} catch (_) {
+			location.reload();
+		}
+	};
 })();
 
 // ===== SECTION: Reload Button =====
 frappe._active_users.reload = {
-  // Create reload icon HTML
-  createIconHTML: function () {
-    return `
+	// Create reload icon HTML
+	createIconHTML: function () {
+		return `
       <span class="reload_icon" data-au-reload="1" title="Reload" style="cursor:pointer;display:inline-block;margin-right:6px;">
         <span class="fa fa-refresh fa-md fa-fw" style="color:#e74c3c;"></span>
       </span>
     `;
-  },
+	},
 
-  // Bind click handler to reload icon
-  bindClickHandler: function ($container) {
-    $container.find('[data-au-reload="1"]').off("click").on("click", function () {
-      window.clearCacheAndReload();
-    });
-  },
+	// Bind click handler to reload icon
+	bindClickHandler: function ($container) {
+		$container
+			.find('[data-au-reload="1"]')
+			.off('click')
+			.on('click', function () {
+				try {
+					console.log('[reload_button.js] bindClickHandler (reload_triggered)');
+					window.clearCacheAndReload();
+				} catch (err) {
+					var errMsg =
+						err && err.message ? err.message.substring(0, 50) : 'reload_error';
+					console.log('[reload_button.js] bindClickHandler (' + errMsg + ')');
+				}
+			});
+	},
 
-  // Initialize reload button in container
-  init: function ($container) {
-    if (!$container || !$container.length) return;
-    this.bindClickHandler($container);
-  }
+	// Initialize reload button in container
+	init: function ($container) {
+		if (!$container || !$container.length) return;
+		this.bindClickHandler($container);
+	},
 };
-
